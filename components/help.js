905ae0858cd938c03f2e22e1f027c027629cbdd4
Carlos Cochero <cocheroc@gmail.com>
2025-10-18 20:30:53 -0500

    Actualizar help.js

--- components/help.js
+++ components/help.js
@@ -5,5 +5,6 @@ export function showHelp() {
   create <description>          Create a new task with the provided description.
   update <id> <description>     Update an existing task by ID.
   mark-task <id> <status>       Mark a task as completed or pending.
+  list <status>.                Show tasks depending of filter(each filter has a color) that is defined by its status.
   help                          Show this help message.`);
 }
